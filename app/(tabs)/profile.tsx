46a2945ab7570f4b930fe981d641761f0ff67e22
Thushan <ktwimalaweera@gmail.com>
2025-04-15 09:57:02 +1000

    profile

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Tab() {
+	return (
+		<View style={styles.container}>
+			<Text>Tab [Home|Profile]</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "white",
+	},
+});
